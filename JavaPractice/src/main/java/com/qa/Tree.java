3caef66f5550c2829b5aa5d5aa435d2f1eee7fa3
Alex Gravener <alexgravener@gmail.com>
2023-10-23 16:26:07 +0100

    Created Tree class with instance variables, and getters and setters

---
+++ JavaPractice/src/main/java/com/qa/Tree.java
@@ -0,0 +1,36 @@
+package com.qa;
+
+public class Tree {
+
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getGenus() {
+        return Genus;
+    }
+
+    public void setGenus(String genus) {
+        this.Genus = genus;
+    }
+
+    public String getCountryOfOrigin() {
+        return CountryOfOrigin;
+    }
+
+    public void setCountryOfOrigin(String countryOfOrigin) {
+        this.CountryOfOrigin = countryOfOrigin;
+    }
+
+    private String Genus;
+    private String CountryOfOrigin;
+
+
+
+}
