5ade82353202668a1c047dc671f1d78a436547cb
ailis-mc <ailismcsweeney09@gmail.com>
2023-10-24 15:55:29 +0100

    arrays and arraylist

--- JavaPractice/src/main/java/com/qa/App.java
+++ JavaPractice/src/main/java/com/qa/App.java
@@ -2,6 +2,9 @@ package com.qa;
 
 import com.tree.Tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -26,5 +29,36 @@ public class App
         Tree.setKingdom("Animalia");
         if (Tree.getKingdom().equals("Animalia"))
             System.out.println("I am an ent");
+
+        Tree readyMadeTrees [] = new Tree[3];
+        readyMadeTrees[0] = sakura;
+        readyMadeTrees[1] = oak;
+        readyMadeTrees[2] = willow;
+
+        System.out.println(Arrays.deepToString(readyMadeTrees));
+
+        Tree[] newTrees = {new Tree("Palm", "Arecaceae",
+                "S America"), new Tree("Birch", "Betula",
+                "N America, N Europe and N Asia")};
+
+        System.out.println(Arrays.deepToString(newTrees));
+
+        ArrayList<Tree> coolTrees = new ArrayList<Tree>();
+        coolTrees.add(sakura);
+        coolTrees.add(willow);
+        coolTrees.add(oak);
+        coolTrees.add(new Tree("Palm", "Arecaceae",
+                "S America"));
+        coolTrees.add(new Tree("Birch", "Betula",
+                "N America, N Europe and N Asia"));
+
+        System.out.println(coolTrees.toString());
+
+        for(Tree tree: coolTrees) {
+            System.out.println(tree.getName());
+        }
+
+
+
     }
 }
